a5705c35e29c70361ab6a8dd12fc676bd8a67395
Pinokiyossh <52872935+Pinokiyossh@users.noreply.github.com>
2019-07-14 11:02:24 +0700

    Create Restart.py

---
+++ Restart.py
@@ -0,0 +1,23 @@
+import random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.2)
+mengetik('Loading...')
+mengetik('Mohon Tunggu...')
+mengetik('...')
+mengetik('...')
+mengetik('...')
+mengetik('...')
+mengetik('...')
+mengetik('...')
+mengetik('Meminta Akses...')
+mengetik('Login Success...')
+mengetik('Selamat Datang Tuan Infected...')
+mengetik('Nama Saya SAFA...')
+mengetik('SYSTEM ATTACK FACEBOOK ACCOUNT')
+mengetik('SAFA Siap Membantu Tuan...')
+mengetik('...')
