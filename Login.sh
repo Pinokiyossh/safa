8f1b6370fc175d4decb7409dcd594fc948e009e2
Pinokiyossh <52872935+Pinokiyossh@users.noreply.github.com>
2019-07-14 12:04:13 +0700

    Update Login.sh

--- Login.sh
+++ Login.sh
@@ -1,9 +1,14 @@
 pkg install update
+clear
 pkg install upgrade
+clear
 pkg install python
+clear
 pkg install python2
+clear
 python2 Security.py
 python Restart.py
+clear
 cd SAFA
 python2 SAFA.py
 cd 
